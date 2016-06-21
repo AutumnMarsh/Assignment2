4d317d9fc0caf2d23c027214be4ff89b831894d3
Autumn Marsh <Autumn Marsh>
2016-06-20 21:40:41 -0700

    Commiting homework files

---
+++ assignment2part1/js/script.js
@@ -0,0 +1,114 @@
+// Assignment 2 Part 1 - Autumn Marsh
+
+// step 1
+/*
+var name = prompt("What is your name?", "First name, last name");
+    alert(name.length);
+
+
+//step 2
+var name, number;
+var name = prompt("What is your first name?", "First name");
+var nameLgth = (name.length) - 1;
+var nmbr = prompt("Enter a number between 0 and " + nameLgth);
+var nmbrChar = (name.charAt(nmbr));
+    alert("Your number " + nmbr + " corresponds to the letter \'" + nmbrChar + "\' in your name.");
+
+
+// step 3
+var firstName = prompt("What is your first name?", "First name");
+var lastName = prompt("Now what is your last name?", "Last name");
+firstName = (firstName + " ");
+var fullName = (firstName.concat(lastName));
+alert("Your name is: " + fullName + ".");
+
+
+// step 4
+var myString = "The quick brown fox jumps over the lazy dog";
+    alert("The index for the word fox is " + (myString.lastIndexOf("fox")) + ".");
+
+
+// step 5
+var myString = "The quick brown fox jumps over the lazy fox";
+    alert(myString.lastIndexOf("fox"));
+
+
+// step 6
+var myString = "The quick brown fox jumps over the lazy dog.";
+var fullName = prompt("What is your full name?", "First name, last name");
+    alert(myString.replace("the lazy dog", fullName));
+
+
+// step 7
+// still totally stumped. this alerts the user's entry but what to match it against?
+var myString = "The quick brown fox jumps over the lazy dog.";
+var myPrompt = "Enter a word, any word!";
+var word = prompt(myPrompt, "Type a word");
+    alert(word);
+    //alert(myString.match(/enter a word, any word/));
+    
+
+
+// step 8
+var old_string = "The quick brown fox jumps over the lazy dog";
+var strIndex = (old_string.indexOf("the lazy dog"));
+var new_string = (old_string.substr(strIndex));
+    alert(new_string.toUpperCase());
+
+
+// step 9
+var myString = "     THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG     ";
+myString = (myString.trim());
+    alert(myString.toLowerCase());
+
+
+// step 10
+var myString = "the quick brown fox jumps over the lazy dog"; 
+var firstLtr = myString.charAt(0); //gets t
+var firstLtr = firstLtr.toUpperCase(); //turn t to T
+var strLength = myString.length;
+myString = myString.slice(1, Number(strLength));
+    alert(firstLtr + myString);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assignment2part2/js/script.js
@@ -0,0 +1,77 @@
+// Assignment 2 Part 2 - Autumn Marsh
+
+// PART 1 ---------------------------
+// step 1
+/*
+var number = prompt("Enter a number", "...");
+    console.log(Math.abs(number));
+ 
+
+// step 2
+var number = prompt("Enter a decimal number", "7.345");
+    console.log(Math.ceil(number));
+
+// step 3
+var number = prompt("Enter a decimal number", "7.345");
+    console.log(Math.floor(number));
+
+
+// step 4 ? Issue stems from multiple numbers
+var text = prompt("Enter 5 numbers separated by decimals", "6, 3, 45, 5, 18");
+//var numSet = (text.match(/(\d[\d\.]*)/g));
+//var numSet = Number(numSet);
+//console.log(typeof (numSet));
+//console.log(Math.min(numSet));
+  
+var a = "text";
+var b = a.split(',').map(function(item) {
+    console.log(parseInt(item, 10));
+});
+
+
+//var numSet = parseInt(numSet, 10);          
+//var minSet = (Math.min(numSet));
+//var maxSet = (Math.max(numSet));
+   // console.log(minSet + maxSet);
+
+
+// step 5
+var aNumber = Number(prompt("Enter a number", "45"));
+    console.log("Your number is the square root of " + aNumber * aNumber);
+
+
+// PART 2 ---------------------------
+// step 6
+var today = new Date();
+    console.log(today.getDate());
+
+
+// step 7 ?
+var thisMonth = new Date();
+console.log(thisMonth.getMonth());
+*/
+
+
+// step 8
+// step 9
+// step 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
